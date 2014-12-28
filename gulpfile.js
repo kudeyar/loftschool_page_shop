6c5129143d135b49bc957071afe65310619f4cdf
Serega <kudeyar-rolevik@yandex.ru>
2014-12-29 00:08:05 +0300

    header complete

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@
 
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    opn = require('opn');
+    opn = require('opn'),
+    wiredep = require('wiredep').stream,
+    livereload = require('gulp-livereload');
 
+//
+gulp.task('wiredep', function () {
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components'
+        }))
+        .pipe(gulp.dest('./app'));
+});
+
+// server
 gulp.task('connect', function() {
     connect.server({
         root: 'app',
@@ -11,6 +23,12 @@ gulp.task('connect', function() {
     });
     opn('http://127.0.0.8:8080/');
 });
+
+//php
+gulp.task('php', function(){
+    gulp.src('app/*.php')
+        .pipe(connect.reload());
+})
 
 //html
 gulp.task('html', function(){
@@ -30,11 +48,17 @@ gulp.task('css', function(){
         .pipe(connect.reload());
 })
 
+gulp.task('watch', function(){
+    livereload.listen();
+    gulp.watch('app/*.php', ['php']).on('change', livereload.changed);
+});
+
 //слежка
 gulp.task('watch', function(){
     gulp.watch(['app/*.html'], ['html']);
     gulp.watch(['app/js/*.js'], ['js']);
     gulp.watch(['app/css/*.css'], ['css']);
+    gulp.watch('bower.json', ['wiredep']);
 })
 
 //default
