a7d1c47bf8b4ddb8e0e610516ab39d68709ac5e3
Serega <kudeyar-rolevik@yandex.ru>
2015-01-11 08:58:50 +0300

    fixed dropdownmenu and mini bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.content__product-slider-linkimg').on('click', function(e){
+    $('.content__product-slider-linkimg').on('click', function (e) {
         e.preventDefault();
 
         var $this = $(this),
@@ -14,15 +14,24 @@ $(document).ready(function(){
             item.addClass('img_active').siblings().removeClass('img_active');
         }
 
-        display.find('img').fadeOut(duration, function(){
+        display.find('img').fadeOut(duration, function () {
             $(this).attr('src', path).fadeIn(duration);
         })
     })
 
-$('.header__bottom-menu-basket').mouseover(function(){
-    $('.header__bottom-menu-dropdownmenu').slideToggle();
-})
+//$('.header__bottom-menu-basket').mouseover(function(){
+//    $('.header__bottom-menu-dropdownmenu').slideToggle();
+//})
 
+    $('.header__bottom-menu-basket').hover(function() {
+            $(this).addClass("active");
+            $(this).find('.header__bottom-menu-dropdownmenu').stop(true, true); // останавливаем всю текущую анимацию
+            $(this).find('.header__bottom-menu-dropdownmenu').slideDown();
+        },
+        function() {
+            $(this).removeClass("active");
+            $(this).find('.header__bottom-menu-dropdownmenu').slideUp('fast');
+        });
 
 
 });
